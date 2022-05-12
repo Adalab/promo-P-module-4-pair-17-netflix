e08487c6811e7b27eb23229292a52685088dde07
LorenaGomezPerez <lorenagomezperez@gmail.com>
2022-05-12 11:25:58 +0200

    localhost 4000

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-// create and config server
+// Habilitamos el servidor para que reciba el Fetch de cualquier dirección
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-// init express aplication
+// Arrancamos el puerto 4000
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get("/api/movies", (req, res) => {
+  const response = {
+    success: true,
+    movies: [
+      {
+        id: "1",
+        title: "Gambita de dama",
+        gender: "Drama",
+        image: "https://via.placeholder.com/150",
+      },
+      {
+        id: "2",
+        title: "Friends",
+        gender: "Comedia",
+        image: "https://via.placeholder.com/150",
+      },
+    ],
+  };
 });
