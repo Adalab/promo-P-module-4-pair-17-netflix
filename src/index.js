566045b6890d72cedabb257d024a3c3524239d30
Rosa Codina <rosa.codina90@gmail.com>
2022-05-13 20:04:37 +0200

    exercise

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Importamos los dos módulos que necesitamos
 
-const express = require("express");
-const cors = require("cors");
-const movies = require("./movies.json");
+const express = require('express');
+const cors = require('cors');
+const movies = require('./movies.json');
 
 //Todo el código que trae el servidor (express) nos lo traemos a la variable server.
 const server = express();
@@ -19,10 +19,15 @@ server.listen(serverPort, () => {
 
 // así atiende peticiones (endpoint)
 
-server.get("/movies", (req, res) => {
+server.get('/movies', (req, res) => {
   const response = {
     success: true,
     movies: movies,
   };
   res.json(response);
 });
+
+const staticServerPathWeb = './src/public-react';
+server.use(express.static(staticServerPathWeb));
+const staticServerImage = './src/public-movies-images';
+server.use(express.static(staticServerImage));
