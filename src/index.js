3c280d7ae5104b58dce879ebe49fd75dfe40680e
LorenaGomezPerez <lorenagomezperez@gmail.com>
2022-05-21 20:03:54 +0200

    add commits

--- src/index.js
+++ src/index.js
@@ -2,36 +2,34 @@
 
 const express = require("express");
 const cors = require("cors");
+const Database = require("better-sqlite3");
 const movies = require("./movies.json");
 const users = require("./users.json");
-const Database = require("better-sqlite3");
 
-//Todo el código que trae el servidor (express) nos lo traemos a la variable server.
+//Todo el código que trae el servidor lo guardamos en la variable server.
 const server = express();
-// Habilitamos el servidor para que reciba Fetch de cualquier direccón (cors)
+
+// Configuración de Express Js para Apis:
+// Nuestro servidor de Espress va a tener en cuenta que pueden venir          peticiones de otro sitio y todas las peticiones van a ser em formato json
 server.use(cors());
-// configuramos express para que las peticiones y respuestas se envíen usando json
 server.use(express.json());
 
-//renderiza una página cualquiera
+// Configuración de Express para dinámicos
 server.set("view engine", "ejs");
 
-// Arrancamos el puerto 4000
+// Configura la base de datos SQLITE
+
+const db = Database("./src/data/database.db", { verbose: console.log });
+
+// Arrancar el servidor
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-// Configura la base de datos en Node JS
-
-const db = Database("./src/data/database.db", { verbose: console.log });
-
-//sacar los datos de la base de datos
-
-// así atiende peticiones (endpoint)
+// ENDPOINTS
 
 server.get("/movies", (req, res) => {
-  //constante que guarda el query de filtrado
   const query = db.prepare(`SELECT * FROM movies`);
   const movies = query.all();
   const genderFilterParam = req.query.gender ? req.query.gender : "";
@@ -82,7 +80,7 @@ server.post("/login", (req, res) => {
 //   return null;
 // });
 
-//endpoint registro de nuevas usuarias en el back
+// registro de nuevas usuarias en el back
 
 server.post("/signup", (req, res) => {
   const query = db.prepare(`SELECT  *
@@ -115,11 +113,15 @@ server.post("/signup", (req, res) => {
 // 5.actualiza el perfil de la usuaria en el back
 
 server.post("/user/profile", (req, res) => {
-  const query = db.prepare(`SELECT  *
-    FROM users
-    WHERE email = ?`);
+  const query = db.prepare(
+    `UPDATE user SET
+      email = ? password = ? WHERE id = ?`
+  );
+  const result = query.run(req.body.email, req.body.password, req.body.id);
+  res.json({ success: true });
+});
 
-// servidor de estáticos
+//  configuración de servidor de estáticos
 const staticServerPathWeb = "./src/public-react";
 server.use(express.static(staticServerPathWeb));
 
@@ -130,4 +132,4 @@ server.use(express.static(staticServerImage));
 //servidor de estáticos para los estilos
 
 const pathServerPublicStyles = "./src/public-css";
-server.user(express.static(pathServerPublicStyles));
+server.use(express.static(pathServerPublicStyles));
